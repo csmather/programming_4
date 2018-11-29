8e808f6c646e641e081c0a33f53feec86a39b279
csmather <44986652+csmather@users.noreply.github.com>
2018-11-28 19:30:17 -0800

    Update multimap_tester.cc

--- multimap_tester.cc
+++ multimap_tester.cc
@@ -9,11 +9,19 @@ int main() {
 	LLRB_multimap<int, int> set;
 	std::vector<int> keys{2, 18, 42, 43, 51, 54, 74, 93, 99};
 
+	// Try to get from empty tree
+	try {
+		std::cout << "Get 1 from empty multimap: "
+		          << set.Get(1) << std::endl;
+	} catch (std::exception &e) {
+		std::cerr << "Error: empty multimap" << std::endl;
+	}
+
 	// Insert a bunch of keys
 	for (auto i : keys) {
+		// Values for keys are (key + 1, + 2)
 		set.Insert(i, i + 1);
 		set.Insert(i, i + 2);
-		set.Insert(i, i + 3);
 	}
 	std::cout << "After insertions:" << std::endl;
 	set.Print();
@@ -23,19 +31,31 @@ int main() {
 	std::cout << "Get 99:" << std::endl;
 	std::cout << set.Get(99) << std::endl;
 
-	// Delete all the keys in another order
+	// Remove all back key values in random order
 	std::random_shuffle(keys.begin(), keys.end());
 	for (auto i : keys) {
 		set.Remove(i);
 	}
-	std::cout << "After deletions:" << std::endl;
+	std::cout << "After value deletions:" << std::endl;
 	set.Print();
 
+	std::cout << "Get 43:" << std::endl;
+	std::cout << set.Get(43) << std::endl;
+	std::cout << "Get 99:" << std::endl;
+	std::cout << set.Get(99) << std::endl;
+
 	// Remove all values of key 2
-	for (int i = 0; i < 3; i++) {
+	for (int i = 0; i < 2; i++) {
 		set.Remove(2);
 	}
 	std::cout << "After deletion of key 2:" << std::endl;
+	set.Print();
+
+	// Remove all keys again to delete nodes
+	for (auto i : keys) {
+		set.Remove(i);
+	}
+	std::cout << "After node deletions: " << std::endl;
 	set.Print();
 
 	return 0;
