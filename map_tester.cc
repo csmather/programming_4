305ad042e4cf155c1755c2e2ee108e8fd9a2ad9e
csmather <44986652+csmather@users.noreply.github.com>
2018-11-28 19:29:41 -0800

    Update map_tester.cc

--- map_tester.cc
+++ map_tester.cc
@@ -6,25 +6,37 @@
 
 // Tester
 int main() {
-    LLRB_map<int, int> set;
-    std::vector<int> keys{2, 18, 42, 43, 51, 54, 74, 93, 99};
+	LLRB_map<int, int> set;
+	std::vector<int> keys{2, 18, 42, 43, 51, 54, 74, 93, 99};
 
-    // Insert a bunch of keys
-    for (auto i : keys) {
-        set.Insert(i, i + 1);
-    }
-    std::cout << "After insertions:" << std::endl;
-    set.Print();
+	// Try to get from empty tree
+	try {
+		std::cout << "Get 1 from empty map: "
+		          << set.Get(1) << std::endl;
+	} catch (std::exception &e){
+		std::cerr << "Error: empty map" << std::endl;
+	}
 
-    std::cout << "Get 43" << std::endl;
-    std::cout << set.Get(43) << std::endl;
-    // Delete all the keys in another order
-    std::random_shuffle(keys.begin(), keys.end());
-    for (auto i : keys) {
-        set.Remove(i);
-    }
-    std::cout << "After deletions:" << std::endl;
-    set.Print();
+	// Insert a bunch of keys
+	for (auto i : keys) {
+		// Value for each key is (key + 1)
+		set.Insert(i, i + 1);
+	}
+	std::cout << "After insertions:" << std::endl;
+	set.Print();
 
-    return 0;
+	std::cout << "Get 43:" << std::endl;
+	std::cout << set.Get(43) << std::endl;
+	std::cout << "Get 99:" << std::endl;
+	std::cout << set.Get(99) << std::endl;
+	
+	// Remove all the keys in random order
+	std::random_shuffle(keys.begin(), keys.end());
+	for (auto i : keys) {
+		set.Remove(i);
+	}
+	std::cout << "After deletions:" << std::endl;
+	set.Print();
+
+	return 0;
 }
